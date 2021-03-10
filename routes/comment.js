294c6a1fa6cfb0267789e8c8989a895a4e8e101e
isaac_lee <yy0221ss@gmail.com>
2021-03-11 03:07:09 +0900

    환자 간호사 코멘트 작업

--- routes/comment.js
+++ routes/comment.js
@@ -2,104 +2,75 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
-router.use(express.static('public'));
 var sanitizeHtml = require('sanitize-html');
 //template 파일을 이용해서 필요한 body값을 넣어주면 공통적인 html 코드를 자동 생성
 var template = require('../lib/template.js');
-var page = require('../lib/page.js')
+var page = require('../lib/page.js');
+const { err } = require('dialog');
 
-
-//코멘트 페이지 기본 환자 설정
-router.get('/', function(request, response) {
-    var title = 'data';
-    var id = 1;
-    var date = new Date();
-    var year = 2021;
-    var month = 3;
-    var day = 1;
-    db.query(`SELECT * FROM patient`, function(error, patients) {
-        response.redirect(`/comment/${patients[0].name}/${year}-${month}-${day}`);
-    })
+router.get('/', function (request, response) {
+  var date = new Date();
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  if (month < 10) {
+    month = "0" + String(month);
+  }
+  if (day < 10) {
+    day = "0" + String(day);
+  }
+  var id = request.session.userid;
+  response.redirect(`/comment/${id}/${year}-${month}-${day}`);
 });
 
-//환자 페이지 기본 날짜 설정
 router.get('/:patientId', function (request, response) {
-    var title = 'data';
-    var id = 1;
-    var date = new Date();
-    var year = 2021;
-    var month = 3;
-    var day = 1;
-    response.redirect(`/comment/${request.params.patientId}/${year}-${month}-${day}`);
-})
-
-//주간 환자 상태 체크 코멘트
-router.get('/:patientId/:date', function (request, response) {
-    var title = 'comment';
-    var id = 1;
-    var comment_box;
-    var year = 2020;
-    var month = 11;
-    var day = 1;
-    var comment_input =
-    `
-    <div class="container" style="text-align: center;">
-        <form action="../comment_process" method="post">
-            <input type="hidden" class="form-control" name="name" value=asd>
-            <input type="hidden" class="form-control" name="year" value=${year}>
-            <input type="hidden" class="form-control" name="month" value=${month}>
-            <input type="hidden" class="form-control" name="day" value=${day}>
-            <br><input type="text" class="form-control" name="comment" placeholder="Comment">
-            <br><button class="btn btn-primary">등록</button>
-        </form>
-    </div>
-    `;
-    console.log(`SELECT comment FROM comment_${year}_${month} WHERE name='${request.params.patientId}' AND day='${day}'`);
-    db.query(`SELECT comment FROM comment_${year}_${month} WHERE name='${request.params.patientId}' AND day='${day}'`, function(error, comment) {
-        if(comment.length == 0){    //코멘트가 없을 경우
-            comment_box = `
-            <div class="card">
-                <div class="card-body">
-                    코멘트가 없습니다
-                </div>
-            </div>
-            `;
-        }else{                      //코멘트가 있을 경우
-            comment_box = template.comment(comment);
-        }
-    });
-    db.query(`SELECT * FROM patient`, function(error, patients) {
-        var list = template.list(patients, request.params.patientId, title);
-        var html = page.HTML(title, id, list,
-            `
-            <div class="col-md-10">
-                <br>
-                <div class="calendar"></div>
-                <script src="../js/calendar.js"></script>
-                ${comment_box}
-                ${comment_input}
-            </div>
-            `
-            //화면에 출력할 html body
-        );
-        response.send(html);
-    });
+  var date = new Date();
+  var year = date.getFullYear();
+  var month = date.getMonth()+1;
+  var day = date.getDate();
+  if (month < 10) {
+    month = "0" + String(month);
+  }
+  if (day < 10) {
+    day = "0" + String(day);
+  }
+  response.redirect(`/comment/${request.params.patientId}/${year}-${month}-${day}`);
 });
 
-router.post('/comment_process', function (req, res) {  
-    //코멘트 등록
-    console.log(req.body.comment);
-    var sql = `INSERT INTO comment_${req.body.year}_${req.body.month} VALUES(?,?,?)`
-    var param = [req.body.name,req.body.day,req.body.comment];
-    db.query(sql, param, function(err, rows) {
-        if (err){
-            console.log(err);
-        }
-        else{
-            console.log(rows.insertId);
-            res.redirect(`/comment/${req.body.name}/${req.body.year}-${req.body.month}-${req.body.day}`);
-        }
-    });
+router.get('/:patientId/:date', function (request, response) {  
+  var title = 'comment';
+  var comment_box;
+  var year = request.params.date.split('-')[0];
+  var month = request.params.date.split('-')[1];
+  var day = request.params.date.split('-')[2];
+  
+  fs.readFile(`data/patients/comment/${request.params.patientId}-${year}-${month}-${day}`, function(error, comments){
+    var comment = '';
+    if (error) {
+      comment_box = `
+      <div class="card">
+          <div class="card-body">
+              코멘트가 없습니다
+          </div>
+      </div>
+      `;
+    } else {
+      comment = JSON.parse(comments);
+      comment_box = template.comment(comment);
+    }
+    var html = page.HTML(title, '간호사', '',
+          `
+          <div class="col-md-10">
+              <br>
+              <div class="calendar"></div>
+              <script src="/public/js/calendar.js"></script>
+              ${comment_box}
+          </div>
+          `
+          //화면에 출력할 html body
+      );
+    response.send(html);
+  });
 });
 
 module.exports = router;
--- routes/nurse_comment.js
+++ routes/nurse_comment.js
@@ -5,7 +5,8 @@ var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 //template 파일을 이용해서 필요한 body값을 넣어주면 공통적인 html 코드를 자동 생성
 var template = require('../lib/template.js');
-var page = require('../lib/page.js')
+var page = require('../lib/page.js');
+const { err } = require('dialog');
 
 router.get('/', function (request, response) {
   var title = 'comment';
@@ -13,6 +14,12 @@ router.get('/', function (request, response) {
   var year = date.getFullYear();
   var month = date.getMonth() + 1;
   var day = date.getDate();
+  if (month < 10) {
+    month = "0" + String(month);
+  }
+  if (day < 10) {
+    day = "0" + String(day);
+  }
   fs.readdir('./data/patients', function(error, filelist){
     var i=0;
     while (i < filelist.length) {
@@ -28,8 +35,14 @@ router.get('/', function (request, response) {
 router.get('/:patientId', function (request, response) {
   var date = new Date();
   var year = date.getFullYear();
-  var month = date.getMonth() + 1;
+  var month = date.getMonth()+1;
   var day = date.getDate();
+  if (month < 10) {
+    month = "0" + String(month);
+  }
+  if (day < 10) {
+    day = "0" + String(day);
+  }
   response.redirect(`/nurse/comment/${request.params.patientId}/${year}-${month}-${day}`);
 });
 
@@ -39,47 +52,57 @@ router.get('/:patientId/:date', function (request, response) {
   var year = request.params.date.split('-')[0];
   var month = request.params.date.split('-')[1];
   var day = request.params.date.split('-')[2];
-  var comment_input =
-  `
-  <div class="container" style="text-align: center;">
-      <form action="../comment_process" method="post">
-          <input type="hidden" class="form-control" name="name" value=${request.params.patientId}>
-          <input type="hidden" class="form-control" name="year" value=${year}>
-          <input type="hidden" class="form-control" name="month" value=${month}>
-          <input type="hidden" class="form-control" name="day" value=${day}>
-          <br><input type="text" class="form-control" name="comment" placeholder="Comment">
-          <br><button class="btn btn-primary">등록</button>
-      </form>
-  </div>
-  `;
-  // TODO 코멘트 부분은 수정해야함
-  var comment = "";
-  if(comment.length == 0){    //코멘트가 없을 경우
-    comment_box = `
-    <div class="card">
-        <div class="card-body">
-            코멘트가 없습니다
-        </div>
-    </div>
-    `;
-  } else {                      //코멘트가 있을 경우
+  
+  fs.readFile(`data/patients/comment/${request.params.patientId}-${year}-${month}-${day}`, function(error, comments){
+    var comment = '';
+    var comment_input =
+    `
+    <div class="container" style="text-align: center;">
+        <form action="../comment_process" method="post">
+            <input type="hidden" class="form-control" name="patientId" value=${request.params.patientId}>
+            <input type="hidden" class="form-control" name="year" value=${year}>
+            <input type="hidden" class="form-control" name="month" value=${month}>
+            <input type="hidden" class="form-control" name="day" value=${day}>`
+    if (error) {
+      comment_box = `
+      <div class="card">
+          <div class="card-body">
+              코멘트가 없습니다
+          </div>
+      </div>
+      `;
+      comment_input += `
+            <br><input type="text" class="form-control" name="comment" placeholder="코멘트를 여기에 작성해주세요."></input>
+            <br><button class="btn btn-primary">등록</button>
+          </form>
+      </div>`
+    } else {
+      comment = JSON.parse(comments);
       comment_box = template.comment(comment);
-  }
-  fs.readdir('./data/patients', function(error, patients){
-    var list = template.list(patients, request.params.patientId, title);
-    var html = page.nurse_HTML(title, '간호사', list,
-        `
-        <div class="col-md-10">
-            <br>
-            <div class="calendar"></div>
-            <script src="/public/js/calendar.js"></script>
-            ${comment_box}
-            ${comment_input}
-        </div>
-        `
-        //화면에 출력할 html body
-    );
+      comment_input +=
+      `
+              <br><input type="text" class="form-control" name="comment" value="${comment.comments}"></input>
+              <br><button class="btn btn-primary">수정</button>
+          </form>
+      </div>
+      `;
+    }
+    fs.readdir('./data/patients', function(error, patients){
+      var list = template.list(patients, request.params.patientId, title);
+      var html = page.nurse_HTML(title, '간호사', list,
+          `
+          <div class="col-md-10">
+              <br>
+              <div class="calendar"></div>
+              <script src="/public/js/calendar.js"></script>
+              ${comment_box}
+              ${comment_input}
+          </div>
+          `
+          //화면에 출력할 html body
+      );
     response.send(html);
+    });
   });
 });
 
@@ -93,11 +116,11 @@ router.post('/comment_process', function (req, res) {
     'comment': req.body.comment
   };
   var content = {
-    "comment" : param.comment
+    "comments" : param.comment
   }
   console.log(param.comment);
   var comment = JSON.stringify(content);
-  fs.writeFile(`data/patients/comment/${param.id}/${param.year}/${param.month}/${param.day}/comment`, comment, 'utf8', function(error){
+  fs.writeFile(`data/patients/comment/${param.id}-${param.year}-${param.month}-${param.day}`, comment, 'utf8', function(error){
     res.redirect(`/nurse/comment/${req.body.name}/${req.body.year}-${req.body.month}-${req.body.day}`);
   });
 });
